e12f88657e086d495a42a29c58b69d18a522ae47
Pascal Veselka <pascal.veselka@ms.bulme.at>
2023-01-03 18:51:57 +0100

    a

--- javascriptsocket.js
+++ javascriptsocket.js
@@ -18,12 +18,36 @@ else
     }
 }
 //---------------------------------------------------------
+//-------------------------Socket--------------------------
+const socket = new WebSocket('ws://localhost:54321');
+
+socket.onopen = function()
+{
+	socket.send('Hello there from javascript');
+    console.log('opened');
+};
+
+socket.onmessage = function(event)
+{
+	console.log(event.data);
+};
+/*import net from "net"
+
 const client = new net.Socket();
-client.connect({port: 50900});
+client.connect({ port: 54321 }, process.argv[2] ?? "localhost", () => {
+    client.write(`${process.argv[3]}\r\n`)
+  })
+
+client.on(net.connect, () => {
+    console.log("Connected");
+    client.write("Hello from client");
+});
 
 client.on("data", (data) =>{
-    
-})
+    console.log('Server says: ${data.toString("utf-8)}');
+});*/
+
+//---------------------------------------------------------
 
 function syncBoard()
 {
@@ -33,6 +57,8 @@ function syncBoard()
 function onResetClick()
 {
     document.body.style.background = 'red';
+    socket.send('reset');
+    console.log('reset');
 }
 
 function showMovesClick()
