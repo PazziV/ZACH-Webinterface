12d4c5b8e17b7ebc0c5e12ec1e263588f5c66633
PazziV <pasiveselka11@gmail.com>
2023-03-10 10:29:39 +0100

    saving game...

--- zachweb.js
+++ zachweb.js
@@ -1,7 +1,7 @@
 //-------Set width/height depending on screen ratio--------
 if(document.documentElement.clientHeight > document.documentElement.clientWidth)
 {
-    var board = document.getElementsByClassName('chessboard');
+    let board = document.getElementsByClassName('chessboard');
     for(i = 0; i < board.length; i++)
     {
         board[i].style.width = '98vw';
@@ -10,39 +10,80 @@ if(document.documentElement.clientHeight > document.documentElement.clientWidth)
 } 
 else
 {
-    var board = document.getElementsByClassName('chessboard');
+    let board = document.getElementsByClassName('chessboard');
     for(i = 0; i < board.length; i++)
     {
         board[i].style.width = '98vh';
         board[i].style.height = '98vh';
     }
 }
+//------------------------Variables------------------------
+const command = {
+    RESET: 0,
+    SYNC: 1,
+    GETMOVES: 2,
+    MOVE: 3
+};
+
+const color = {
+    BLACK: 0,
+    WHITE: 1,
+    blank: 2
+};
+
+let msgArray;
+
+const playField = ["A8","B8","C8","D8","E8","F8","G8","H8",
+                "A7","B7","C7","D7","E7","F7","G7","H7",
+                "A6","B6","C6","D6","E6","F6","G6","H6",
+                "A5","B5","C5","D5","E5","F5","G5","H5",
+                "A4","B4","C4","D4","E4","F4","G4","H4",
+                "A3","B3","C3","D3","E3","F3","G3","H3",
+                "A2","B2","C2","D2","E2","F2","G2","H2",
+                "A1","B1","C1","D1","E1","F1","G1","H1"];
 
 //-------------------------Socket--------------------------
-var socket = new WebSocket("ws://192.168.111.232:9002");
+const socket = new WebSocket("ws://192.168.100.232:9002");
 
 socket.onopen = function(e) {
-    alert("[open] Connection established");
-    alert("Sending to server");
+    console.log("[open] Connection established");
+    //alert("Sending to server");
     socket.send("My name is John");
 };
-  
-socket.onmessage = function(event) {
-    alert(`[message] Data received from server: ${event.data}`);
 
+socket.onmessage = function(event) {
+    let message = event.data;
+    console.log("[message] Data received from server: " + message);
     
+    msgArray = message.split(" ");
+
+    switch(msgArray[0])
+    {
+        case command.RESET:
+
+            break;
+        case command.SYNC:
+            
+            break;
+        case command.GETMOVES:
+
+            break;
+        case command.MOVE: 
+            movePieceDBClick();
+            break;
+    }
 };
   
 socket.onclose = function(event) {
     if (event.wasClean) {
-      alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+      alert("[close] Connection closed cleanly, code=" + event.code + "reason=" + event.reason);
     } else {
-      alert('[close] Connection died');
+      alert("[close] Connection died");
     }
 };
   
 socket.onerror = function(error) {
-    alert(`[error]`);
+    alert("[ERROR]");
 };
 
 //---------------------------------------------------------
@@ -50,22 +91,19 @@ socket.onerror = function(error) {
 function syncBoard()
 {
     socket.send("syncBoard");
-
 }
 
 function onResetClick()
 {
-    document.body.style.background = 'red';
     socket.send("Reset");
 }
 
-function showMovesClick()
+function fieldClick()
 {
-    document.body.style.background = 'green';
+    
     socket.send("Show Moves");
-}
 
-function movePieceDBClick()
-{
-
+    // let alt = "A2"; let neu = "A3";
+    // document.getElementById(neu).src = document.getElementById(alt).src;
+    // document.getElementById(alt).src = "";
 }
