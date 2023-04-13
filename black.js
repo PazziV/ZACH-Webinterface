d4588dfdaa791f7adc14d5bb8a10477d8cf35758
Pascal Veselka <pascal.veselka@ms.bulme.at>
2023-04-13 13:07:51 +0200

    yes

--- black.js
+++ black.js
@@ -1,23 +1,3 @@
-//-------Set width/height depending on screen ratio--------
-if(document.documentElement.clientHeight > document.documentElement.clientWidth)
-{
-    let board = document.getElementsByClassName('chessboard');
-    for(i = 0; i < board.length; i++)
-    {
-        board[i].style.width = '98vw';
-        board[i].style.height = '98vw';
-    }
-} 
-else
-{
-    let board = document.getElementsByClassName('chessboard');
-    for(i = 0; i < board.length; i++)
-    {
-        board[i].style.width = '98vh';
-        board[i].style.height = '98vh';
-    }
-}
-
 //------------------------Variables------------------------
 const command = {
     RESET: 0,
